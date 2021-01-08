782a3571376b0e81927cbf556c40d64a4c63fb33
liqlvnvn <liqlvnvn@gmail.com>
2021-01-08 20:24:39 +0300

    Working on GUI

--- gui.py
+++ gui.py
@@ -1,25 +1,56 @@
-import tkinter as tk
+#!/usr/bin/python3
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+from tkinter import *
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+root = Tk()
+root.title("Basketball Statistics")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+def open():
+    game_details_window = Toplevel()
+    game_details_window.title("Game Details")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+game_details_button = Button(text="Game Details", command=open)
+game_details_button.grid(row=0, column=1)
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+myLabel = Label(root, text="Hello World!")
+myLabel.grid(row=0, column=0)
+
+made_bucket_button = Button(text="Made Bucket")
+made_bucket_button.grid(row=1, column=0)
+
+made_turnover_button = Button(text="Turnover")
+made_turnover_button.grid(row=2, column=0)
+
+
+rebound_button = Button(text="Rebound")
+blockshot_button = Button(text="Block")
+steal_button = Button(text="Steal")
+#grid(row=0, column=0)
+
+
+rosters_frame = LabelFrame(root, text="Rosters", padx=5, pady=5)
+rosters_frame.grid(row=100, column=0)
+
+team1_frame = LabelFrame(rosters_frame, text="Team1", padx=20, pady=50)
+team1_frame.grid(row=0, column=0)
+team1_player1_checkbox = Checkbutton(team1_frame, text="Player1")
+team1_player1_checkbox.pack()
+
+team2_frame = LabelFrame(rosters_frame, text="Team2", padx=20, pady=50)
+team2_frame.grid(row=0, column=100)
+team2_player1_checkbox = Checkbutton(team2_frame, text="Player1")
+team2_player1_checkbox.pack()
+
+test_team1_button = Button(rosters_frame, text="Team1")
+test_team2_button = Button(rosters_frame, text="Team2")
+test_team1_button.grid(row=0, column=1)
+test_team2_button.grid(row=0, column=2)
+
+event_list_frame = LabelFrame(root, text="Events", padx=50, pady=50)
+event_list_frame.grid(row=100, column=1)
+test_event1_button = Label(event_list_frame, text="Event1")
+test_event2_button = Label(event_list_frame, text="Event2")
+test_event1_button.pack()
+test_event2_button.pack()
+
+root.mainloop()

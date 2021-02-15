63c1a9b82ef7861207fbafe5c2592e29a7b83966
liqlvnvn <liqlvnvn@gmail.com>
2021-02-15 21:43:13 +0300

    Fixing roster - playing roster interaction

--- gui/gui.py
+++ gui/gui.py
@@ -57,15 +57,22 @@ class MainWindow():
         foul_button.grid(row=6, column=0)
 
 
+
         rosters_frame = LabelFrame(root, text="Rosters", padx=5, pady=5)
         rosters_frame.grid(row=100, column=0)
+
+        self.team1_roster_list = []
+        self.team2_roster_list = []
+        self.team1_playing_list = []
+        self.team2_playing_list = []
 
         self.team1_frame = LabelFrame(rosters_frame, text="Team1 roster", padx=5, pady=5)
         self.team1_frame.grid(row=0, column=0)
         self.team1_frame_roster = LabelFrame(self.team1_frame, text="Roster")
         team1_add_player_button = Button(self.team1_frame, text="+",
-                command=lambda x = self.team1_frame_roster:
-                            self.add_player2roster(x))
+                command=lambda x = self.team1_frame_roster,
+                               y = self.team1_roster_list:
+                            self.add_player2roster(x, y))
         team1_add_player_button.pack()
         self.team1_frame_roster.pack()
         team1_player1_checkbox = Checkbutton(self.team1_frame_roster, text="Player1")
@@ -81,8 +88,9 @@ class MainWindow():
         self.team2_frame.grid(row=0, column=100)
         self.team2_frame_roster = LabelFrame(self.team2_frame, text="Roster")
         team2_add_player_button = Button(self.team2_frame, text="+",
-                command=lambda x = self.team2_frame_roster:
-                            self.add_player2roster(x))
+                command=lambda x = self.team2_frame_roster,
+                               y = self.team2_roster_list:
+                            self.add_player2roster(x, y))
         team2_add_player_button.pack()
         self.team2_frame_roster.pack()
         team2_player1_checkbox = Checkbutton(self.team2_frame_roster, text="Player5")
@@ -97,22 +105,30 @@ class MainWindow():
         self.playing_roster_frame = LabelFrame(rosters_frame, text="Playing rosters", padx=5, pady=5)
         self.playing_roster_frame.grid(row=0, column=2)
         r = IntVar()
-        self.playing_roster_team1_frame = LabelFrame(self.playing_roster_frame, text="Team1")
-        self.playing_roster_team1_frame.grid(row=0, column=0)
-        self.playing_roster_team2_frame = LabelFrame(self.playing_roster_frame, text="Team2")
-        self.playing_roster_team2_frame.grid(row=0, column=1)
-        test_team11_radiobutton = Radiobutton(self.playing_roster_frame, text="Player1", variable=r, value = 1)
-        test_team12_radiobutton = Radiobutton(self.playing_roster_frame, text="Player2", variable=r, value = 2)
-        test_team13_radiobutton = Radiobutton(self.playing_roster_frame, text="Player3", variable=r, value = 2)
-        test_team21_radiobutton = Radiobutton(self.playing_roster_frame, text="Player4", variable=r, value = 2)
-        test_team22_radiobutton = Radiobutton(self.playing_roster_frame, text="Player5", variable=r, value = 2)
-        test_team23_radiobutton = Radiobutton(self.playing_roster_frame, text="Player6", variable=r, value = 2)
+        self.playing_roster_team1_frame = LabelFrame(self.playing_roster_frame,
+                text="Team1")
+        self.playing_roster_team1_frame.pack() #grid(row=0, column=0)
+        self.playing_roster_team2_frame = LabelFrame(self.playing_roster_frame,
+                text="Team2")
+        self.playing_roster_team2_frame.pack() #grid(row=0, column=1)
+        test_team11_radiobutton = Radiobutton(self.playing_roster_team1_frame,
+                text="Player1", variable=r, value = 1)
+        test_team12_radiobutton = Radiobutton(self.playing_roster_team1_frame,
+                text="Player2", variable=r, value = 2)
+        test_team13_radiobutton = Radiobutton(self.playing_roster_team1_frame,
+                text="Player3", variable=r, value = 2)
+        test_team21_radiobutton = Radiobutton(self.playing_roster_team2_frame,
+                text="Player4", variable=r, value = 2)
+        test_team22_radiobutton = Radiobutton(self.playing_roster_team2_frame,
+                text="Player5", variable=r, value = 2)
+        test_team23_radiobutton = Radiobutton(self.playing_roster_team2_frame,
+                text="Player6", variable=r, value = 2)
         test_team11_radiobutton.grid(row=0, column=0)
         test_team12_radiobutton.grid(row=1, column=0)
         test_team13_radiobutton.grid(row=2, column=0)
-        test_team21_radiobutton.grid(row=0, column=1)
-        test_team22_radiobutton.grid(row=1, column=1)
-        test_team23_radiobutton.grid(row=2, column=1)
+        test_team21_radiobutton.grid(row=0, column=0)
+        test_team22_radiobutton.grid(row=1, column=0)
+        test_team23_radiobutton.grid(row=2, column=0)
 
         event_list_frame = LabelFrame(root, text="Events", padx=5, pady=5)
         event_list_frame.grid(row=100, column=1)
@@ -120,6 +136,10 @@ class MainWindow():
         test_event2_button = Label(event_list_frame, text="Event2")
         test_event1_button.pack()
         test_event2_button.pack()
+
+        substitution_button = Button(game_actions_frame, text="Substitution",
+                command=self.substitution())
+        substitution_button.grid(row=7, column=0)
 
     def new_game(self):
         for widget in self.team1_frame_roster.winfo_children():
@@ -135,11 +155,33 @@ class MainWindow():
     def open_players_database(self):
         e = player_database.PlayerDatabase()
 
-    def add_player2roster(self, frame):
+    def add_player2roster(self, frame, t_list):
         # open window with list of players
         s = select_player.PlayerSelection()
-        p = s.player.name()
-        Checkbutton(frame, text=p).pack()
+        p = s.player
+        t_list = t_list.append(p)
+        Checkbutton(frame, text=p.name()).pack()
+
+    def substitution(self):
+        for widget in self.playing_roster_team1_frame.winfo_children():
+            widget.destroy()
+
+        for widget in self.playing_roster_team1_frame.winfo_children():
+            widget.destroy()
+
+        for player in self.team1_roster_list:
+            if player not in self.team1_playing_list:
+                self.team1_playing_list.append(player)
+
+        for player in self.team2_roster_list:
+            if player not in self.team2_playing_list:
+                self.team2_playing_list.append(player)
+
+        for player in self.team1_playing_list:
+            Radiobutton(self.playing_roster_team1_frame, text=player.name())
+
+        for player in self.team2_playing_list:
+            Radiobutton(self.playing_roster_team2_frame, text=player.name())
 
 
 main_window = MainWindow(root)
